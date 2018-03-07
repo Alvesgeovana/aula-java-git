2b4bfecd3abe8132b59b2316637be8e6b641fab1
04121255283 <04121255283@JIPA-LABIN03-08.ifro.local>
2018-03-07 15:32:20 -0400

    calculadora

---
+++ calculadora/src/br/edu/ifro/calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+/**
+ *
+ * @author 04121255283
+ */
+public class calculadora {
+  public double soma (double num1,double num2){
+     return num1 + num2;
+ }   
+public double subtração (double num1, double num2){
+    return num1- num2;
+}
+public double divisão (double num1, double num2 ){
+    return num1 /num2;
+}
+public double multiplicação (double num1, double num2 ){
+    return num1 * num2;
+}
+}    
+
