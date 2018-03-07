1a147b66e42d0eebfb12e9be059fca45b3e90002
04121255283 <04121255283@JIPA-LABIN03-08.ifro.local>
2018-03-07 14:58:00 -0400

    calculadora

---
+++ ProjetoGitJava/src/br/edu/ifro/calculadora.java
@@ -0,0 +1,21 @@
+
+package br.edu.ifro;
+
+/**
+ *
+ * @author 04121255283
+ */
+public class calculadora {
+    public double soma (double num1,double num2){
+     return num1 + num2;
+ }   
+public double subtração (double n1, double n2){
+    return n1- n2;
+}
+public double divisão (double nu1, double nu2 ){
+    return nu1 /nu2;
+}
+public double multiplicação (double numero1, double numero2 ){
+    return numero1 * numero2;
+}
+}
